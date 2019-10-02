f0b6497b94c1e6690dd32193d5edf02c850acbe4
Reece Kenney <reecekenney50@gmail.com>
2019-10-02 21:13:42 +0100

    registerPage

---
+++ register.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Reeceflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+    </head>
+    <body>
+        hotdogs
+    </body>
+</html>
