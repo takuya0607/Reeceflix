247f13acc03fd46543b6e96dfcd7c01254bb2172
Reece Kenney <reecekenney50@gmail.com>
2019-10-04 13:39:04 +0100

    FormSanitizerClass

--- register.php
+++ register.php
@@ -1,18 +1,11 @@
 <?php
+require_once("includes/classes/FormSanitizer.php");
+
     if(isset($_POST["submitButton"])) {
         
-        $firstName = sanitizeFormString($_POST["firstName"]);
-        echo $firstName;
+        $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
 
-    }
 
-    function sanitizeFormString($inputText) {
-        $inputText = strip_tags($inputText);
-        $inputText = str_replace(" ", "", $inputText);
-        //$inputText = trim($inputText);
-        $inputText = strtolower($inputText);
-        $inputText = ucfirst($inputText);
-        return $inputText;
     }
 ?>
 <!DOCTYPE html>
