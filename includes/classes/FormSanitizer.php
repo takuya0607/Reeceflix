54a99ae4bea66f46fd9299164206f091b16e2249
Reece Kenney <reecekenney50@gmail.com>
2019-10-07 09:16:21 +0100

    sanitationMethods

--- includes/classes/FormSanitizer.php
+++ includes/classes/FormSanitizer.php
@@ -10,5 +10,22 @@ class FormSanitizer {
         return $inputText;
     }
 
+    public static function sanitizeFormUsername($inputText) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
+
+    public static function sanitizeFormPassword($inputText) {
+        $inputText = strip_tags($inputText);
+        return $inputText;
+    }
+
+    public static function sanitizeFormEmail($inputText) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
+
 }
 ?>
